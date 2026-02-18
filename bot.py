1ffe949dd89b166f45793969dc482da660981d0f
Yash Raj Bhatnagar <bhatnagaryashraj984@gmail.com>
2026-02-18 21:09:26 +0530

    removal of CHANNEL_ID

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
