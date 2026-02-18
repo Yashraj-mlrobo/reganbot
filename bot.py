b12f93c5232a67067c5817d18bf29eaec552919b
Yash Raj Bhatnagar <bhatnagaryashraj984@gmail.com>
2026-02-18 21:53:47 +0530

    setting innit file as exception

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ last_link = None
 # ===== LOAD COGS =====
 async def load_extensions():
     for file in os.listdir("./commands"):
-        if file.endswith(".py"):
+        if file.endswith(".py") and file != "__init__.py":
             await bot.load_extension(f"commands.{file[:-3]}")
             print(f"Loaded {file}")
 
